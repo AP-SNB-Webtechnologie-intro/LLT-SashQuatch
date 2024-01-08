c47393158e684b887bd09a67524f32dd93401f52
S150547 <145353100+SashQuatch@users.noreply.github.com>
2024-01-08 14:10:34 +0100

    nieuwe api uitgewerkt.

--- api.js
+++ api.js
@@ -1,17 +1,50 @@
-const axios = require('axios');
+const data = null;
 
-const options = {
-  method: 'GET',
-  url: 'https://esport-matches.p.rapidapi.com/tournament/1',
-  headers: {
-    'X-RapidAPI-Key': '17fdfafc83mshb4ad8cc3f745875p1146eejsnba0147bcd16b',
-    'X-RapidAPI-Host': 'esport-matches.p.rapidapi.com'
+const xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener('readystatechange', function () {
+  if (this.readyState === this.DONE) {
+
+    const allGames = JSON.parse(this.responseText);
+
+    const shuffledGames = shuffle(allGames);
+
+    // Display only 5 random games
+    const randomGames = shuffledGames.slice(0, 5);
+
+    const gameTableBody = document.getElementById("gameTableBody");
+
+    randomGames.forEach(game => {
+      const row = document.createElement("tr");
+      row.innerHTML = `
+        <td>${game.title}</td>
+        <td>${game.genre}</td>
+        <td>${game.platform}</td>
+        <td>${game.publisher}</td>
+        <td>${game.developer}</td>
+        <td>${game.release_date}</td>
+      `;
+      gameTableBody.appendChild(row);
+    });
   }
-};
+});
 
-try {
-	const response = await axios.request(options);
-	console.log(response.data);
-} catch (error) {
-	console.error(error);
+xhr.open('GET', 'https://free-to-play-games-database.p.rapidapi.com/api/games?category=shooter');
+xhr.setRequestHeader('X-RapidAPI-Key', '070c28b493msh1f10b494c1c9ec2p193d69jsnb30f553826de');
+xhr.setRequestHeader('X-RapidAPI-Host', 'free-to-play-games-database.p.rapidapi.com');
+xhr.send(data);
+
+// Function to shuffle an array (Fisher-Yates algorithm)
+function shuffle(array) {
+  let currentIndex = array.length, randomIndex;
+
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
 }
